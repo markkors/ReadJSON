f77cab702e9c7c48be642a5ed7cf31a7734a97ac
markkors <m.kors@rocmn.nl>
2022-02-23 14:54:57 +0100

    Add project files.

---
+++ ReadJSON/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadJSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window ,INotifyPropertyChanged
+
+    {
+
+        private WebRequest httpRequest;
+        private List<cStation> _stations = new List<cStation>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            getWeatherData();
+            cmbStation.SelectionChanged += CmbStation_SelectionChanged;
+        }
+
+        private void CmbStation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedStation  = (cStation)cmbStation.SelectedItem;
+            NotifyPropertyChanged("SelectedStation");
+        }
+
+        public void getWeatherData()
+        {
+            string url = "https://data.buienradar.nl/2.0/feed/json";
+            httpRequest = WebRequest.Create(url);
+            httpRequest.BeginGetResponse(new AsyncCallback(FinishWebRequest), null);
+        }
+
+        private void FinishWebRequest(IAsyncResult ar)
+        {
+            WebResponse resp = httpRequest.EndGetResponse(ar);
+            string r = null;
+            using (StreamReader str = new StreamReader(resp.GetResponseStream()))
+            {
+                
+
+                
+                r = str.ReadToEnd();
+                
+                JObject o = JObject.Parse(r);
+                JToken JSONStations = o.SelectToken("actual.stationmeasurements");
+                foreach(JToken item in JSONStations)
+                {
+                    Debug.WriteLine(item);
+                    cStation station = JsonConvert.DeserializeObject<cStation>(item.ToString());
+                    _stations.Add(station);
+
+                }
+
+            }
+
+        }
+
+        public List<cStation> stations
+        {
+            get
+            {
+                return _stations;
+            }
+            set
+            {
+                _stations = value;
+            }
+        }
+
+        public cStation SelectedStation { get; set; }
+
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ReadJSON/cStation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJSON
+{
+    public class cStation
+    {
+
+        public string stationid { get; set; }
+        public string stationname { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string regio { get; set; }
+        public string timestamp { get; set; }
+        public string weatherdescription { get; set; }
+        public string iconurl { get; set; }
+        public string graphurl { get; set; }
+        public string winddirection { get; set; }
+        public double windgusts { get; set; }
+        public double windspeed { get; set; }
+        public int windBft { get; set; }
+        public double winddirectiondegrees { get; set; }
+        public double temperature { get; set; }
+        public double feeltemperature { get; set; }
+        public double groundtemperature { get; set; }
+        public double humidity { get; set; }
+        public double precipitation { get; set; }
+        public double sunpower { get; set; }
+        public double rainFallLast24Hour { get; set; }
+        public double rainFallLastHour { get; set; }
+    }
+       
+       
+}
